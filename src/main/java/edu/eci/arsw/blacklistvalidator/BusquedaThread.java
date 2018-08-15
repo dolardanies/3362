0f00c611bb542e285b01e3e856daf29f7da6a85d
unknown <2104481@labinfo.is.escuelaing.edu.co>
2018-08-15 09:18:18 -0500

    Miercoles 9:18

--- src/main/java/edu/eci/arsw/blacklistvalidator/BusquedaThread.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/BusquedaThread.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.arsw.blacklistvalidator;
 
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
 import java.util.LinkedList;
 
 /**
@@ -13,18 +14,17 @@ import java.util.LinkedList;
  */
 public class BusquedaThread extends Thread{
     
-    private static final int BLACK_LIST_ALARM_COUNT=5;
     int num_ini;
     int num_fin;
     LinkedList<Integer> OcurrenciasListaNegra=new LinkedList<>();
     int num_ocurrencias;
+    HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
     
     @Override
     public void run(){
-        for (int i=num_ini; i<=num_fin && num_ocurrencias<BLACK_LIST_ALARM_COUNT;i++){
+        for (int i=num_ini; i<=num_fin;i++){
             System.out.println("Busqueda de un segmento del conjunto de servidores");
             if (skds.isInBlackListServer(i, ipaddress)){
-                
                 OcurrenciasListaNegra.add(i);
                 num_ocurrencias++;
             }
@@ -32,7 +32,7 @@ public class BusquedaThread extends Thread{
         }
     }
     
-    public void rangos(int ini, int fin){
+    public void busqueda(int ini, int fin){
         this.num_ini=ini;
         this.num_fin=fin;
         System.out.println("Rango de inicio a fin de la lista total");
